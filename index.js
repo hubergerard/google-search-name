da0b29d2a0d130b4048895178e80bc54d59846d1
Huber GERARD <huber.gerard1@gmail.com>
2020-12-04 10:04:57 +0100

    add maxTabs limit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const maxTabs = 5;
 
 function handleFileSelect(evt) {
     const files = evt.target.files; 
-    for (let i = startIndex, f ; f = files[i] ; i++) {
+    for (let i = 0, f ; f = files[i] ; i++) {
         const reader = new FileReader();
         reader.onload = (function (file) {
             return function (e) {
-                const prospects = $.csv.toObjects(e.target.result).slice(0, maxTabs);
+                const prospects = $.csv.toObjects(e.target.result).slice(startIndex, startIndex + maxTabs);
                 for (prospect of prospects) {
                     window.open(searchUrl + prospect.Prenom + '+' + prospect.Nom + '+' + prospect.Profil.replace(/[^a-zA-Z0-9 ]/g, '').split(' ').join('+'), '_blank');
                 }
