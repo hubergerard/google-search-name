8bd0f7d11beda9e14ae34ea671a35b4dcff004dc
Huber GERARD <huber.gerard1@gmail.com>
2020-12-04 10:31:24 +0100

    startIndex added

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-const keywords = "lead dev"
-const searchUrl = 'https://www.google.fr/search?q='
-const startIndex = 0; 
-const maxTabs = 5; 
+const SEARCH_URL = 'https://www.google.fr/search?q='
+const START_INDEX = 0; 
+const MAX_TABS = 10; 
+const KEYWORDS = 'lead dev'
+
+const FIRST_NAME_FIELD = 'Prenom';
+const LAST_NAME_FIELD = 'Nom';
+const JOB_FIELD = 'Profil';
 
 function handleFileSelect(evt) {
     const files = evt.target.files; 
     for (let i = 0, f ; f = files[i] ; i++) {
         const reader = new FileReader();
-        reader.onload = (function (file) {
-            return function (e) {
-                const prospects = $.csv.toObjects(e.target.result).slice(startIndex, startIndex + maxTabs);
-                for (prospect of prospects) {
-                    window.open(searchUrl + prospect.Prenom + '+' + prospect.Nom + '+' + prospect.Profil.replace(/[^a-zA-Z0-9 ]/g, '').split(' ').join('+'), '_blank');
-                }
-            };
-        })(f);
+        reader.onload = readFileAndOpenTabs(f);
         reader.readAsText(f);
     }
+}
+
+function readFileAndOpenTabs () {
+    return (event) => {
+        const prospects = $.csv.toObjects(event.target.result).slice(START_INDEX, START_INDEX + MAX_TABS);
+        for (prospect of prospects) {
+            openTab(prospect);
+        }
+    }
+}
+
+function openTab(prospect) {
+    window.open(SEARCH_URL + prospect[FIRST_NAME_FIELD] + '+' + prospect[LAST_NAME_FIELD] + '+' + prospect[JOB_FIELD].replace(/[^a-zA-Z0-9 ]/g, '').split(' ').join('+'), '_blank');
 }
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
